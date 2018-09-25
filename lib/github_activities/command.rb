af2d67da4a54109620a8a1dfafb8426476b8bd80
nyaahara <d.niihara@gmail.com>
2018-09-25 16:43:09 +0900

    issues in milestones

--- lib/github_activities/command.rb
+++ lib/github_activities/command.rb
@@ -1,6 +1,7 @@
 require 'github_activities'
 require 'thor'
 require 'github_activities/comment_and_reaction'
+require 'github_activities/milestones'
 
 module GithubActivities
   class Command < Thor
@@ -12,6 +13,16 @@ module GithubActivities
     desc '', '特定リポジトリ、対象ユーザーのコメントとリアクションを返します'
     def get
       hello = GithubActivities::CommentAndReaction.new
+      hello.get(options)
+    end
+
+    option :login_user, aliases: ['l', 'u'], required: true
+    option :password, aliases: 'p', required: true
+    option :repository, aliases: 'r', required: true
+    option :milestone, aliases: 'm', required: true
+    desc '', '特定リポジトリのマイルストーン内のすべてを返します'
+    def milestones
+      hello = GithubActivities::Milestones.new
       hello.get(options)
     end
   end
---
+++ lib/github_activities/milestones.rb
@@ -0,0 +1,25 @@
+require 'github_activities'
+require 'octokit'
+require 'csv'
+
+module GithubActivities
+  class Milestones
+    def get(options)
+      Octokit.configure do |c|
+        c.login = options[:login_user]
+        c.password = options[:password]
+      end
+
+      repository = options[:repository]
+      milestone = options[:milestone]
+      client = Octokit::Client.new
+      list_issues = client.list_issues(repository, state: :all, milestone: milestone, accept: 'application/vnd.github.symmetra-preview+json')
+      CSV.open("#{milestone}.csv", 'w') do |csv|
+        csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考"]
+        list_issues.each do |issue|
+          csv << ['', issue.user.login, issue.title, issue.html_url, '','','',]
+        end
+      end
+    end
+  end
+end
