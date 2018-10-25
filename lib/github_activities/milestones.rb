d53febfb514c38ac67efd73599bf884a805379cd
nyaahara <d.niihara@gmail.com>
2018-10-25 14:22:21 +0900

    new

--- lib/github_activities/milestones.rb
+++ lib/github_activities/milestones.rb
@@ -17,10 +17,35 @@ module GithubActivities
       CSV.open("#{milestone}.csv", 'w') do |csv|
         csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考", '確認方法', 'リリースノート', '目的', 'テストケース' "issue内容"]
         list_issues.each do |issue|
-          /### The person in charge\n\*?(?<person_in_charge>.*).*### How to test to sprint review\n\*?(?<test_procedure>.*).*### Release note\n(?<release_note>.*).*### Purpose\n(?<purpose>.*).*### Testcases\n(?<test_cases>.*)$/m =~ issue.body
+          /### The person in charge\*?(?<person_in_charge>.*).*### How to test to sprint review\*?(?<test_procedure>.*).*### Release note(?<release_note>.*).*### Purpose(?<purpose>.*).*### Testcases(?<testcases>.*)$/m =~ issue.body
           csv << ['', issue.user.login, issue.title, issue.html_url, person_in_charge&.strip, '', '', test_procedure, release_note, purpose, testcases, issue.body]
         end
       end
     end
   end
 end
+# require 'github_activities'
+# require 'octokit'
+# require 'csv'
+#
+# module GithubActivities
+#   class Milestones
+#     def get(options)
+#       Octokit.configure do |c|
+#         c.login = 'smartyoyaku'
+#         c.password = 'yoyaku489'
+#       end
+#
+#       repository = options[:repository]
+#       milestone = options[:milestone]
+#       client = Octokit::Client.new
+#       list_issues = client.list_issues('smartyoyaku/ec', state: :all, milestone: 101, accept: 'application/vnd.github.symmetra-preview+json')
+#       CSV.open("#{milestone}.csv", 'w') do |csv|
+#         csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考", "issue内容"]
+#         list_issues.each do |issue|
+#           csv << ['', issue.user.login, issue.title, issue.html_url, '','','', issue.body,]
+#         end
+#       end
+#     end
+#   end
+# end
--- lib/github_activities/version.rb
+++ lib/github_activities/version.rb
@@ -1,3 +1,3 @@
 module GithubActivities
-  VERSION = "0.4.1"
+  VERSION = "0.4.4"
 end
