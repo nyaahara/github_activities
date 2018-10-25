a07c6aa4c901ad51849eb0efd2d4cf1054213d51
nyaahara <d.niihara@gmail.com>
2018-10-25 13:49:34 +0900

    テンプレつくったので、それごとに抜く感じになるのです。

--- lib/github_activities/milestones.rb
+++ lib/github_activities/milestones.rb
@@ -15,9 +15,10 @@ module GithubActivities
       client = Octokit::Client.new
       list_issues = client.list_issues(repository, state: :all, milestone: milestone, accept: 'application/vnd.github.symmetra-preview+json')
       CSV.open("#{milestone}.csv", 'w') do |csv|
-        csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考", "issue内容"]
+        csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考", '確認方法', 'リリースノート', '目的', 'テストケース' "issue内容"]
         list_issues.each do |issue|
-          csv << ['', issue.user.login, issue.title, issue.html_url, '','','', issue.body,]
+          /### The person in charge\n\*?(?<person_in_charge>.*).*### How to test to sprint review\n\*?(?<test_procedure>.*).*### Release note\n(?<release_note>.*).*### Purpose\n(?<purpose>.*).*### Testcases\n(?<test_cases>.*)$/m =~ issue.body
+          csv << ['', issue.user.login, issue.title, issue.html_url, person_in_charge.strip, '', '', test_procedure, release_note, purpose, testcases, issue.body]
         end
       end
     end
