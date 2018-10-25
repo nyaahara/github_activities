369a559b60a3e1432030eaf31fc2c8fe23407814
nyaahara <d.niihara@gmail.com>
2018-10-25 13:55:19 +0900

    nilguard

--- lib/github_activities/milestones.rb
+++ lib/github_activities/milestones.rb
@@ -18,7 +18,7 @@ module GithubActivities
         csv << ["スプリントRv前確認", "Author", "案件概要", "プルリクエスト（修正コード）", "確認担当", "リリース判定", "備考", '確認方法', 'リリースノート', '目的', 'テストケース' "issue内容"]
         list_issues.each do |issue|
           /### The person in charge\n\*?(?<person_in_charge>.*).*### How to test to sprint review\n\*?(?<test_procedure>.*).*### Release note\n(?<release_note>.*).*### Purpose\n(?<purpose>.*).*### Testcases\n(?<test_cases>.*)$/m =~ issue.body
-          csv << ['', issue.user.login, issue.title, issue.html_url, person_in_charge.strip, '', '', test_procedure, release_note, purpose, testcases, issue.body]
+          csv << ['', issue.user.login, issue.title, issue.html_url, person_in_charge&.strip, '', '', test_procedure, release_note, purpose, testcases, issue.body]
         end
       end
     end
